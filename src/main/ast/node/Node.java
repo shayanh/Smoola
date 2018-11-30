1d92ddd259afbd4c7416ebbf2d91ee5f2b48d9ad
Armita <armt.sf@gmail.com>
2018-11-30 15:53:15 +0330

    added line number + tests

--- src/main/ast/node/Node.java
+++ src/main/ast/node/Node.java
@@ -3,5 +3,10 @@ package ast.node;
 import ast.Visitor;
 
 public abstract class Node {
+    public int line;
+
+    public int getLine() { return line; }
+    public void setLine(int l) { line = l; }
+
     public void accept(Visitor visitor) {}
 }
