d229c66d3acc70f7dad26981bb92881c92e084b1
Shayan Hosseini <shayan.hosseiny@gmail.com>
2018-11-30 13:15:57 +0330

    pass added, init symbolTable

--- src/main/ast/VisitorImpl.java
+++ src/main/ast/VisitorImpl.java
@@ -9,140 +9,172 @@ import ast.node.expression.Value.BooleanValue;
 import ast.node.expression.Value.IntValue;
 import ast.node.expression.Value.StringValue;
 import ast.node.statement.*;
+import symbolTable.SymbolTable;
 
 public class VisitorImpl implements Visitor {
 
+    private Pass pass;
+    private SymbolTable symbolTable;
+
+    public VisitorImpl() {
+        pass = Pass.First;
+        symbolTable = new SymbolTable();
+    }
+
+    public void setPass(Pass newPass) {
+        pass = newPass;
+    }
+
     @Override
     public void visit(Program program) {
-        System.out.println(program.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(program.toString());
         program.getMainClass().accept(this);
-        for (ClassDeclaration classDec: program.getClasses()) {
+        for (ClassDeclaration classDec : program.getClasses()) {
             classDec.accept(this);
         }
     }
 
     @Override
     public void visit(ClassDeclaration classDeclaration) {
-        System.out.println(classDeclaration.toString());
-        for (VarDeclaration varDec: classDeclaration.getVarDeclarations()) {
+        if (pass == Pass.PrintOrder)
+            System.out.println(classDeclaration.toString());
+        for (VarDeclaration varDec : classDeclaration.getVarDeclarations()) {
             varDec.accept(this);
         }
-        for (MethodDeclaration methodDec: classDeclaration.getMethodDeclarations()) {
+        for (MethodDeclaration methodDec : classDeclaration.getMethodDeclarations()) {
             methodDec.accept(this);
         }
     }
 
     @Override
     public void visit(MethodDeclaration methodDeclaration) {
-        System.out.println(methodDeclaration.toString());
-        for (VarDeclaration arg: methodDeclaration.getArgs()) {
+        if (pass == Pass.PrintOrder)
+            System.out.println(methodDeclaration.toString());
+        for (VarDeclaration arg : methodDeclaration.getArgs()) {
             arg.accept(this);
         }
-        for (VarDeclaration localVar: methodDeclaration.getLocalVars()) {
+        for (VarDeclaration localVar : methodDeclaration.getLocalVars()) {
             localVar.accept(this);
         }
-        for (Statement statement: methodDeclaration.getBody()) {
+        for (Statement statement : methodDeclaration.getBody()) {
             statement.accept(this);
         }
     }
 
     @Override
     public void visit(VarDeclaration varDeclaration) {
-        System.out.println(varDeclaration.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(varDeclaration.toString());
         varDeclaration.getIdentifier().accept(this);
     }
 
     @Override
     public void visit(ArrayCall arrayCall) {
-        System.out.println(arrayCall.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(arrayCall.toString());
         arrayCall.getInstance().accept(this);
         arrayCall.getIndex().accept(this);
     }
 
     @Override
     public void visit(BinaryExpression binaryExpression) {
-        System.out.println(binaryExpression.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(binaryExpression.toString());
         binaryExpression.getLeft().accept(this);
         binaryExpression.getRight().accept(this);
     }
 
     @Override
     public void visit(Identifier identifier) {
-        System.out.println(identifier.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(identifier.toString());
     }
 
     @Override
     public void visit(Length length) {
-        System.out.println(length.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(length.toString());
         length.getExpression().accept(this);
     }
 
     @Override
     public void visit(MethodCall methodCall) {
-        System.out.println(methodCall.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(methodCall.toString());
         methodCall.getInstance().accept(this);
         methodCall.getMethodName().accept(this);
-        for (Expression arg: methodCall.getArgs()) {
+        for (Expression arg : methodCall.getArgs()) {
             arg.accept(this);
         }
     }
 
     @Override
     public void visit(NewArray newArray) {
-        System.out.println(newArray.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(newArray.toString());
         newArray.getExpression().accept(this);
     }
 
     @Override
     public void visit(NewClass newClass) {
-        System.out.println(newClass.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(newClass.toString());
         newClass.getClassName().accept(this);
     }
 
     @Override
     public void visit(This instance) {
-        System.out.println(instance.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(instance.toString());
     }
 
     @Override
     public void visit(UnaryExpression unaryExpression) {
-        System.out.println(unaryExpression.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(unaryExpression.toString());
         unaryExpression.getValue().accept(this);
     }
 
     @Override
     public void visit(BooleanValue value) {
-        System.out.println(value.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(value.toString());
     }
 
     @Override
     public void visit(IntValue value) {
-        System.out.println(value.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(value.toString());
     }
 
     @Override
     public void visit(StringValue value) {
-        System.out.println(value.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(value.toString());
     }
 
     @Override
     public void visit(Assign assign) {
-        System.out.println(assign.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(assign.toString());
         assign.getlValue().accept(this);
         assign.getrValue().accept(this);
     }
 
     @Override
     public void visit(Block block) {
-        System.out.println(block.toString());
-        for (Statement statement: block.getBody()) {
+        if (pass == Pass.PrintOrder)
+            System.out.println(block.toString());
+        for (Statement statement : block.getBody()) {
             statement.accept(this);
         }
     }
 
     @Override
     public void visit(Conditional conditional) {
-        System.out.println(conditional.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(conditional.toString());
         conditional.getExpression().accept(this);
         conditional.getConsequenceBody().accept(this);
         if (conditional.getAlternativeBody() != null) {
@@ -152,14 +184,16 @@ public class VisitorImpl implements Visitor {
 
     @Override
     public void visit(While loop) {
-        System.out.println(loop.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(loop.toString());
         loop.getCondition().accept(this);
         loop.getBody().accept(this);
     }
 
     @Override
     public void visit(Write write) {
-        System.out.println(write.toString());
+        if (pass == Pass.PrintOrder)
+            System.out.println(write.toString());
         write.getArg().accept(this);
     }
 }
