ec487257217f387f959431c59d3ba623b620445d
Shayan Hosseini <shayan.hosseiny@gmail.com>
2018-12-13 16:50:38 +0330

    NoType added

---
+++ src/main/ast/Type/NoType.java
@@ -0,0 +1,13 @@
+package ast.Type;
+
+public class NoType extends Type {
+    @Override
+    public String toString() {
+        return "NoType";
+    }
+
+    @Override
+    public boolean subtype(Type t) {
+        return true;
+    }
+}
